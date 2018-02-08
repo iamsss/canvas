6129da8a29898f57a672a14364083c2cb8a4eaf6
Saurav Kumar <saurav9760@gmail.com>
2018-02-08 22:41:59 +0530

    Adding Slider to Canvas

--- cic.js
+++ cic.js
@@ -1,12 +1,12 @@
 
     
 const canvas = document.getElementById("myCanvas");
-const ctx = canvas.getContext("2d");
-var x=10, y=10, vw=220,vh=277;
-canvas.width = 240;
-canvas.height = 297;
-
 var img = document.getElementById("scream");
+const ctx = canvas.getContext("2d");
+var x=10, y=10, vw=img.width,vh=img.height;
+canvas.width = 250;
+canvas.height = 250;
+
 
 img.addEventListener('click',function(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -35,3 +35,27 @@ function drawImg(ctx,x,y,vw,vh) {
 window.onload = function() {
     drawImg(ctx,x,y,vw,vh);
 };
+
+
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value;
+var v=50;
+
+slider.oninput = function() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    console.log('clicked');
+    console.log(vw);
+    var val = this.value - v;
+   if((vw >= 250 && vh >= 250) || val>0) {
+    vw += val*10;
+    vh += val*10;
+   }
+    
+    drawImg(ctx,x,y,vw,vh);
+    
+    output.innerHTML = this.value;
+    v = this.value;
+  }
+
+  
