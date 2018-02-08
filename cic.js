0895412f2fff344f4e816159fc6a1bf32596ca73
Saurav Kumar <saurav9760@gmail.com>
2018-02-08 21:48:16 +0530

    Click Handle on Canvas

--- cic.js
+++ cic.js
@@ -1,8 +1,37 @@
+
+    
+const canvas = document.getElementById("myCanvas");
+const ctx = canvas.getContext("2d");
+var x=10, y=10, vw=220,vh=277;
+canvas.width = 240;
+canvas.height = 297;
+
+var img = document.getElementById("scream");
+
+img.addEventListener('click',function(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    console.log('clicked');
+    console.log(vw);
+    vw -= 10;
+    vh -= 10;
+    drawImg(ctx,x,y,vw,vh);
+})
+
+img.addEventListener('mouseover',function(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    console.log('clicked');
+    console.log(vw);
+    vw -= 10;
+    vh -= 10;
+    drawImg(ctx,x,y,vw,vh);
+})
+
+function drawImg(ctx,x,y,vw,vh) {
+    ctx.drawImage(img, x, y, vw, vh);
+}
+
+    
+
 window.onload = function() {
-    var canvas = document.getElementById("myCanvas");
-    canvas.width = 240;
-    canvas.height = 297;
-    var ctx = canvas.getContext("2d");
-    var img = document.getElementById("scream");
-    ctx.drawImage(img, 10, 10, 220, 277);
+    drawImg(ctx,x,y,vw,vh);
 };
